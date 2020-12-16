d1e98c550a12ca35f95cc3b7cac5268ebf2e2cdc
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-16 17:28:38 -0600

    WIP refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -4,50 +4,61 @@ require './lib/board'
 class Game
   attr_reader :computer_board,
               :player_board,
-              :computer_plays,
-              :player_plays
+              :computer_ships,
+              :player_ships
 
   def initialize
     @computer_board = Board.new
     @player_board = Board.new
-    @computer_plays = {
-      cruiser: Ship.new("Cruiser", 3),
-      submarine: Ship.new("Submarine", 2)
-    }
-    @player_plays = {
-      cruiser: Ship.new("Cruiser", 3),
-      submarine: Ship.new("Submarine", 2)
-    }
+    @computer_ships = {
+                        cruiser: Ship.new("Cruiser", 3),
+                        submarine: Ship.new("Submarine", 2)
+                      }
+    @player_ships = {
+                      cruiser: Ship.new("Cruiser", 3),
+                      submarine: Ship.new("Submarine", 2)
+                    }
   end
-
-  def start
+  def greeting
     puts "Welcome to BATTLESHIP"
     puts "Enter p to play. Enter q to quit"
     input = gets.chomp.downcase
+    start(input)
+  end
+
+  def start(input)
     if input == "p"
       play_game
     elsif input == "q"
       puts "You have quit the game."
     else
       puts "Please try again."
-      start
+      start(gets.chomp.downcase)
     end
   end
 
-  def play_game
+  def place_all_ships
     computer_ship_placement
     player_ship_placement
+  end
+
+  def continue_firing
+    display_board
+    player_fired_upon(@player_board.cells.keys.sample(1).first)
+    computer_fired_upon
+  end
+
+  def play_game
+    place_all_ships
     loop do
-      if @computer_plays[:cruiser].sunk? && @computer_plays[:submarine].sunk?
+      if @computer_ships[:cruiser].sunk? && @computer_ships[:submarine].sunk?
         p "You won!"
         break
-      elsif @player_plays[:cruiser].sunk? && @player_plays[:submarine].sunk?
+      elsif @player_ships[:cruiser].sunk? && @player_ships[:submarine].sunk?
         p "I won!"
         break
       else
-        display_board
-        player_fired_upon(@player_board.cells.keys.sample(1).first)
-        computer_fired_upon
+        continue_firing
       end
     end
   end
@@ -59,18 +70,22 @@ class Game
 
   def computer_cruiser_placement
     loop do
-      randomized = @computer_board.cells.keys.shuffle![0..2]
-      if @computer_board.valid_placement?(@computer_plays[:cruiser], randomized)
-        return @computer_board.place(@computer_plays[:cruiser], randomized)
+      randomized = random_computer_board_coordinate(0..2)
+      if @computer_board.valid_placement?(@computer_ships[:cruiser], randomized)
+        return @computer_board.place(@computer_ships[:cruiser], randomized)
       end
     end
   end
 
+  def random_computer_board_coordinate(range)
+    randomized = @computer_board.cells.keys.shuffle![range]
+  end
+
   def computer_submarine_placement
     loop do
-      randomized = @computer_board.cells.keys.shuffle![0..1]
-      if @computer_board.valid_placement?(@computer_plays[:submarine], randomized)
-        return @computer_board.place(@computer_plays[:submarine], randomized)
+      randomized = random_computer_board_coordinate(0..1)
+      if @computer_board.valid_placement?(@computer_ships[:submarine], randomized)
+        return @computer_board.place(@computer_ships[:submarine], randomized)
       end
     end
   end
@@ -83,10 +98,15 @@ class Game
     puts @computer_board.render()
   end
 
-  def player_ship_placement
+  def player_placement_intro
     p "I have laid out my ships on the grid."
     p "You now need to lay out your two ships."
-    p "The Cruiser is three units long and the Submarine is two units long."
+    print "The #{@player_ships[:cruiser].name} is #{@player_ships[:cruiser].length} units long and "
+    print "the #{@player_ships[:submarine].name} is #{@player_ships[:submarine].length} units long.\n"
+  end
+
+  def player_ship_placement
+    player_placement_intro
     print_player_board
     player_cruiser_instructions
     player_cruiser_placement
@@ -99,15 +119,14 @@ class Game
     print "> "
   end
 
+  end
   def player_cruiser_placement
      player_input = gets.chomp.upcase.split(" ")
-     if player_input.count == 3 && @player_board.valid_placement?(@player_plays[:cruiser], player_input)
-       @player_board.place(@player_plays[:cruiser], player_input)
-
+     if player_input.count == 3 && @player_board.valid_placement?(@player_ships[:cruiser], player_input)
+       @player_board.place(@player_ships[:cruiser], player_input)
        print_player_board
      else
-       p "Those are invalid coordinates. Please try again:"
-       print "> "
+       invalid_coordinate_alert
        player_cruiser_placement
      end
   end
@@ -119,15 +138,17 @@ class Game
 
   def player_submarine_placement
     player_input = gets.chomp.upcase.split(" ")
-    if player_input.count == 2 && @player_board.valid_placement?(@player_plays[:submarine], player_input)
-      @player_board.place(@player_plays[:submarine], player_input)
-
-
+    if player_input.count == 2 && @player_board.valid_placement?(@player_ships[:submarine], player_input)
+      @player_board.place(@player_ships[:submarine], player_input)
     else
-      p "Those are invalid coordinates. Please try again:"
-      print "> "
+      invalid_coordinate_alert
       player_submarine_placement
     end
+  end
+
+  def invalid_coordinate_alert
+    p "Those are invalid coordinates. Please try again:"
+    print "> "
   end
 
   def display_board
@@ -137,21 +158,26 @@ class Game
     print_player_board
   end
 
-  # computer firing on player board
   def player_fired_upon(coordinate)
     if !@player_board.cells[coordinate].fired_upon?
-      @player_board.cells[coordinate].fire_upon
-      if @player_board.cells[coordinate].render == "M"
-        p "My shot on #{coordinate} was a miss."
-      elsif @player_board.cells[coordinate].render == "H"
-        p "My shot on #{coordinate} was a direct hit."
-      elsif @player_board.cells[coordinate].render == "X"
-        p "My shot on #{coordinate} sunk your ship."
-      end
+      fire_upon_player(coordinate)
     else
       player_fired_upon(@player_board.cells.keys.sample(1).first)
     end
   end
+
+
+  def fire_upon_player(coordinate)
+    @player_board.cells[coordinate].fire_upon
+    if @player_board.cells[coordinate].render == "M"
+      p "My shot on #{coordinate} was a miss."
+    elsif @player_board.cells[coordinate].render == "H"
+      p "My shot on #{coordinate} was a direct hit."
+    elsif @player_board.cells[coordinate].render == "X"
+      p "My shot on #{coordinate} sunk your ship."
+    end
+  end
+
 
   # player firing on computer board
   def computer_fired_upon
@@ -183,5 +209,3 @@ class Game
       computer_fired_upon
     end
   end
-
-end
--- test/game_test.rb
+++ test/game_test.rb
@@ -23,8 +23,8 @@ class GameTest < Minitest::Test
   def test_game_has_computer_ships
     game = Game.new
 
-    computer_cruiser = game.computer_plays[:cruiser]
-    computer_submarine = game.computer_plays[:submarine]
+    computer_cruiser = game.computer_ships[:cruiser]
+    computer_submarine = game.computer_ships[:submarine]
 
     assert_instance_of Ship, computer_cruiser
     assert_instance_of Ship, computer_submarine
@@ -32,8 +32,8 @@ class GameTest < Minitest::Test
 
   def test_game_has_player_ships
     game = Game.new
-    player_cruiser_placement = game.player_plays[:cruiser]
-    player_submarine_placement = game.player_plays[:submarine]
+    player_cruiser_placement = game.player_ships[:cruiser]
+    player_submarine_placement = game.player_ships[:submarine]
 
     assert_instance_of Ship, player_cruiser_placement
     assert_instance_of Ship, player_submarine_placement
